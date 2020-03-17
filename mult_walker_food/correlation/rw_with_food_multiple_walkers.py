f77712170adcb3c513f469c875b73543526143e1
Sebastian2306 <sebastian.steinhaeuser@hhu.de>
2020-03-17 20:12:51 +0100

    hallo

--- mult_walker_food/correlation/rw_with_food_multiple_walkers.py
+++ mult_walker_food/correlation/rw_with_food_multiple_walkers.py
@@ -101,21 +101,21 @@ def rw(L, N, pot, f): #boxsize, number of walkers, time, food-parameter L=int*10
             rn = rand.random()
             #print(rn)
             if rn < prob[n,0]:
-                lr_wall[n] = lr_wall.copy()[n] + ((pos.copy()[n,0] + 1) // L)
-                pos[n,0] = (pos[n,0] + 1) % L
-                evec[n,0] = 1
-            if rn > prob[n,0] and rn < prob[n,0] + prob[n,1]:
-                lr_wall[n] = lr_wall.copy()[n] + ((pos.copy()[n,0] - 1) // L)
-                pos[n,0] = (pos[n,0] - 1) % L
-                evec[n,0] = -1
-            if rn > prob[n,0] + prob[n,1] and rn < 1 - prob[n,3]:
-                ud_wall[n] = ud_wall.copy()[n] + ((pos.copy()[n,1] + 1) // L)
+                lr_wall[n] = lr_wall.copy()[n] + ((pos.copy()[n,1] + 1) // L)
                 pos[n,1] = (pos[n,1] + 1) % L
                 evec[n,1] = 1
-            if rn > 1 - prob[n,3]:
-                ud_wall[n] = ud_wall.copy()[n] + ((pos.copy()[n,1] - 1) // L)
+            if rn > prob[n,0] and rn < prob[n,0] + prob[n,1]:
+                lr_wall[n] = lr_wall.copy()[n] + ((pos.copy()[n,1] - 1) // L)
                 pos[n,1] = (pos[n,1] - 1) % L
                 evec[n,1] = -1
+            if rn > prob[n,0] + prob[n,1] and rn < 1 - prob[n,3]:
+                ud_wall[n] = ud_wall.copy()[n] + ((pos.copy()[n,0] + 1) // L)
+                pos[n,0] = (pos[n,0] + 1) % L
+                evec[n,0] = 1
+            if rn > 1 - prob[n,3]:
+                ud_wall[n] = ud_wall.copy()[n] + ((pos.copy()[n,0] - 1) // L)
+                pos[n,0] = (pos[n,0] - 1) % L
+                evec[n,0] = -1
         #print(pos)
         #print('lr:', lr_wall)
         #print('ud:', ud_wall)
@@ -127,8 +127,8 @@ def rw(L, N, pot, f): #boxsize, number of walkers, time, food-parameter L=int*10
             mat[int(pos[n,1]), int(pos[n,0])] = 0 #set entry on which a walker sits to zero, so allways pacman eats all the food
         #print(mat)
         if t in grid:
-            deltax = (pos[:,0] - startconf[:,0]) + lr_wall * L
-            deltay = (pos[:,1] - startconf[:,1]) + ud_wall * L          
+            deltax = (pos[:,1] - startconf[:,1]) + lr_wall * L
+            deltay = (pos[:,0] - startconf[:,0]) + ud_wall * L          
             msd[:,i] = deltax**2 + deltay**2
             fpercentage[i] = sum(sum(mat)) / (f * L**2)
             #fdimension[i] = fractal_dimension(mat, f)
@@ -190,36 +190,36 @@ def multiple_walks(L, N, pot, f, n):#n number of walks
     
 #f = int(sys.argv[1])
     
-#avgmsd, grid, fpercentage, diffmsd, corr1, corr2, escp = multiple_walks(100, 1000, 1, 5, 10)
-fmats = np.zeros((10,100,100))
-for k in range(10):
-    fmats[k] = rw(100, 1000, 2, 5)[4][33]
-    
-np.savetxt('fmats2.dat', fmats.flatten())
-#diff_avg = diffmsd.copy()   
+avgmsd, grid, fpercentage, diffmsd, corr1, corr2, escp = multiple_walks(100, 1000, 3, 5, 100)
+#fmats = np.zeros((10,100,100))
+#for k in range(10):
+#    fmats[k] = rw(100, 1000, 2, 5)[4][20]
+#    
+#np.savetxt('fmats2.dat', fmats.flatten())
+#diff_avg = diff.copy()   
 #for k in range(2,len(diff)-2):
 #    diff_avg[k] = (diff[k-2] + diff[k-1] + diff[k] + diff[k+1] + diff[k+2])/5.0
     
     
 #plt.figure(num=None, figsize=(6, 10), dpi=80, facecolor='w', edgecolor='k')
-#plt.subplot(3,1,1)
+#plt.subplot(4,1,1)
 #plt.loglog(grid,avgmsd,label='$F=5$',lw = 0.5)
 #plt.loglog(grid,grid,label='$reference$',lw = 0.5)
 #plt.title('$0\%\ density$')
 ##plt.xlabel('$steps$', size = 15)
 #plt.ylabel('$msd$', size = 15)
 #plt.legend()
-#plt.subplot(3,1,2)
+#plt.subplot(4,1,2)
 #plt.plot(grid,fpercentage)
 #plt.xscale('log')
 ##plt.xlabel('$steps$', size = 15)
 #plt.ylabel('$food$', size = 15)
-##plt.subplot(4,1,3)
-##plt.plot(grid,corr1[:,0,0])
-##plt.xscale('log')
-##plt.xlabel('$steps$', size = 15)
-##plt.ylabel('$corr$', size = 15)
-#plt.subplot(3,1,3)
+#plt.subplot(4,1,3)
+#plt.plot(grid,escp)
+#plt.xscale('log')
+#plt.xlabel('$steps$', size = 15)
+#plt.ylabel('$stepautocorr$', size = 15)
+#plt.subplot(4,1,4)
 #plt.plot(grid, diffmsd / avgmsd * grid, label='$forward\ difference$')
 ##plt.plot(grid, diff_avg / avgmsd * grid, label='$local\ mean$')
 #plt.legend()
@@ -227,10 +227,9 @@ np.savetxt('fmats2.dat', fmats.flatten())
 #plt.xlabel('$steps$', size = 15)
 #plt.ylabel('$diff\ exponent$', size = 15)
 #plt.savefig('0dens.pdf')
-#np.savetxt('msd0.dat', avgmsd)
-#np.savetxt('food0.dat', fpercentage)
-#np.savetxt('diff0.dat', diffmsd)
-#np.savetxt('corr1_0.dat', corr1.flatten())
-#np.savetxt('corr2_0.dat', corr2.flatten())
-#np.savetxt('escp0.dat', escp)
-
+np.savetxt('msd10.dat', avgmsd)
+np.savetxt('food10.dat', fpercentage)
+np.savetxt('diff10.dat', diffmsd)
+np.savetxt('corr1_10.dat', corr1.flatten())
+np.savetxt('corr2_10.dat', corr2.flatten())
+np.savetxt('escp10.dat', escp)
